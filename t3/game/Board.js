7bbaaf3829a4da9aed6671f78a275018fd925b16
Pedro Silva <p21silva98@outlook.com>
2019-01-05 18:33:29 +0000

    board refactor

--- t3/game/Board.js
+++ t3/game/Board.js
@@ -23,8 +23,19 @@ class Board extends CGFobject {
 
         //Board Matrix
         this.matrix = []
+        this.cellWidth = 3;
+        this.halfCell = this.cellWidth/2.0;
+        this.oneCellAndHalf = this.cellWidth+this.halfCell;
+        this.twoCellAndHalf = (2.0*this.cellWidth)+this.halfCell;
+        this.threeCellAndHalf = (3.0*this.cellWidth)+this.halfCell;
+        this.fourCellAndHalf = (4.0*this.cellWidth)+this.halfCell;
+        this.fiveCellAndHalf = (5.0*this.cellWidth)+this.halfCell;
+        this.sixCellAndHalf = (6.0*this.cellWidth)+this.halfCell;
+        this.smallDelta = 0.0005;
         this.init();
         this.loaded = true;
+
+        
     }
 
     init(){
@@ -33,39 +44,39 @@ class Board extends CGFobject {
         let pickingId = 15;
         //First few lines
         let line1 = emptyLine.slice();
-        line1[3] = new Cell(this.scene, -1.5,-19.5,1.5,-16.5,pickingId);
+        line1[3] = new Cell(this.scene, -this.halfCell,-this.sixCellAndHalf,this.halfCell,-this.fiveCellAndHalf,pickingId);
         pickingId++;
         this.matrix.push(line1);
 
         let line2 = emptyLine.slice();
-        line2[2] = new Cell(this.scene, -4.5,-16.5,-1.5,-13.5,pickingId);
+        line2[2] = new Cell(this.scene, -this.oneCellAndHalf,-this.fiveCellAndHalf,-this.halfCell,-this.fourCellAndHalf,pickingId);
         pickingId++;
-        line2[3] = new Cell(this.scene, -1.5,-16.5,1.5,-13.5,pickingId);
+        line2[3] = new Cell(this.scene, -this.halfCell,-this.fiveCellAndHalf,this.halfCell,-this.fourCellAndHalf,pickingId);
         pickingId++;
-        line2[4] = new Cell(this.scene, 1.5,-16.5,4.5,-13.5,pickingId);
+        line2[4] = new Cell(this.scene, this.halfCell,-this.fiveCellAndHalf,this.oneCellAndHalf,-this.fourCellAndHalf,pickingId);
         pickingId++;
         this.matrix.push(line2);
 
         let line3 = emptyLine.slice();
-        line3[1] = new Cell(this.scene, -7.5,-13.5,-4.5,-10.5,pickingId);
+        line3[1] = new Cell(this.scene, -this.twoCellAndHalf,-this.fourCellAndHalf,-this.oneCellAndHalf,-this.threeCellAndHalf,pickingId);
         pickingId++;
-        line3[2] = new Cell(this.scene, -4.5,-13.5,-1.5,-10.5,pickingId);
+        line3[2] = new Cell(this.scene, -this.oneCellAndHalf,-this.fourCellAndHalf,-this.halfCell,-this.threeCellAndHalf,pickingId);
         pickingId++;
-        line3[3] = new Cell(this.scene, -1.5,-13.5,1.5,-10.5,pickingId);
+        line3[3] = new Cell(this.scene, -this.halfCell,-this.fourCellAndHalf,this.halfCell,-this.threeCellAndHalf,pickingId);
         pickingId++;
-        line3[4] = new Cell(this.scene, 1.5,-13.5,4.5,-10.5,pickingId);
+        line3[4] = new Cell(this.scene, this.halfCell,-this.fourCellAndHalf,this.oneCellAndHalf,-this.threeCellAndHalf,pickingId);
         pickingId++;
-        line3[5] = new Cell(this.scene, 4.5,-13.5,7.5,-10.5,pickingId);
+        line3[5] = new Cell(this.scene, this.oneCellAndHalf,-this.fourCellAndHalf,this.twoCellAndHalf,-this.threeCellAndHalf,pickingId);
         pickingId++;
         this.matrix.push(line3);
 
         //Create the middle tiles
-        let sizeX = 3;
-        let sizeY = 3;
+        let sizeX = this.cellWidth;
+        let sizeY = this.cellWidth;
 
-        for(let i = -10.5; i < 10.5 - 0.0005; i += sizeY){
+        for(let i = -this.threeCellAndHalf; i < this.threeCellAndHalf - this.smallDelta; i += sizeY){
             let line = []
-            for(let j = -10.5; j < 10.5 - 0.0005; j+= sizeX){
+            for(let j = -this.threeCellAndHalf; j < this.threeCellAndHalf - this.smallDelta; j+= sizeX){
                 line.push(new Cell(this.scene, j,i,j+sizeX, i+sizeY,pickingId));
                 pickingId++;
             }
@@ -75,31 +86,31 @@ class Board extends CGFobject {
         //Create the bottom tiles
         let last3 = emptyLine.slice();
         
-        last3[1] = new Cell(this.scene, -7.5,10.5,-4.5,13.5,pickingId);
+        last3[1] = new Cell(this.scene, -this.twoCellAndHalf,this.threeCellAndHalf,-this.oneCellAndHalf,this.fourCellAndHalf,pickingId);
         pickingId++;
-        last3[2] = new Cell(this.scene, -4.5,10.5,-1.5,13.5,pickingId);
+        last3[2] = new Cell(this.scene, -this.oneCellAndHalf,this.threeCellAndHalf,-this.halfCell,this.fourCellAndHalf,pickingId);
         pickingId++;
-        last3[3] = new Cell(this.scene, -1.5,10.5,1.5,13.5,pickingId);
+        last3[3] = new Cell(this.scene, -this.halfCell,this.threeCellAndHalf,this.halfCell,this.fourCellAndHalf,pickingId);
         pickingId++;
-        last3[4] = new Cell(this.scene, 1.5,10.5,4.5,13.5,pickingId);
+        last3[4] = new Cell(this.scene, this.halfCell,this.threeCellAndHalf,this.oneCellAndHalf,this.fourCellAndHalf,pickingId);
         pickingId++;
-        last3[5] = new Cell(this.scene, 4.5,10.5,7.5,13.5,pickingId);
+        last3[5] = new Cell(this.scene, this.oneCellAndHalf,this.threeCellAndHalf,this.twoCellAndHalf,this.fourCellAndHalf,pickingId);
         pickingId++;
         this.matrix.push(last3)
 
         let last2 = emptyLine.slice()
         
-        last2[2] = new Cell(this.scene, -4.5,13.5,-1.5,16.5,pickingId);
+        last2[2] = new Cell(this.scene, -this.oneCellAndHalf,this.fourCellAndHalf,-this.halfCell,this.fiveCellAndHalf,pickingId);
         pickingId++;
-        last2[3] = new Cell(this.scene, -1.5,13.5,1.5,16.5,pickingId);
+        last2[3] = new Cell(this.scene, -this.halfCell,this.fourCellAndHalf,this.halfCell,this.fiveCellAndHalf,pickingId);
         pickingId++;
-        last2[4] = new Cell(this.scene, 1.5,13.5,4.5,16.5,pickingId);
+        last2[4] = new Cell(this.scene, this.halfCell,this.fourCellAndHalf,this.oneCellAndHalf,this.fiveCellAndHalf,pickingId);
         pickingId++;
         this.matrix.push(last2)
 
         let last1 = emptyLine.slice()
         
-        last1[3] = new Cell(this.scene, -1.5,16.5,1.5,19.5,pickingId);
+        last1[3] = new Cell(this.scene, -this.halfCell,this.fiveCellAndHalf,this.halfCell,this.sixCellAndHalf,pickingId);
         pickingId++;
         this.matrix.push(last1);
         this.isLoaded = true;
@@ -115,11 +126,13 @@ class Board extends CGFobject {
         for(var i = 0 ; i < validMoves.length; i++){
             let x = validMoves[i].destX;
             let y = validMoves[i].destY;
-            //console.log("Coordinates of the tile:" + x + " " + y)
             this.activateTile(x,y);
         }
     }
 
+    /**
+     * Deactivates all the tiles
+     */
     deactivateTiles(){
         for(var i = 0 ; i < this.activatedTiles.length; i++){
             this.activatedTiles[i].deactivate();
@@ -127,10 +140,14 @@ class Board extends CGFobject {
         this.activatedTiles = [];
     }
 
+    /**
+     * Activates the tile in the coordinates
+     * @param {X coordinate} x 
+     * @param {Y coordinate} y 
+     */
     activateTile(x,y){
         for(var i = 0; i < this.matrix.length; i++){
             for(var j = 0 ; j < this.matrix[i].length; j++){
-                //console.log(this.matrix[i][j])
                 let xTile = this.matrix[i][j].x;
                 let yTile = this.matrix[i][j].y;
                 if(x == xTile && y == yTile){
@@ -147,13 +164,9 @@ class Board extends CGFobject {
 
         if(!this.scene.graph.loadedOk)
             return;
-            
-        let index = 15;
 
         //Display all the light color tiles =========================================
         this.scene.graph.materials['dark tiles'].apply();
-
-        //All whites besides matrix        
 
         //Main Matrix
         for(var i = 0 ; i < this.matrix.length ; i++){
@@ -174,8 +187,6 @@ class Board extends CGFobject {
 
         //Display all the dark color tiles ===========================================
         this.scene.graph.materials['light tiles'].apply();
-
-        //All blacks besides matrix
 
         //Main Matrix
         for(var i = 0 ; i < this.matrix.length ; i++){
